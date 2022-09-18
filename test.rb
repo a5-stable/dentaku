e435f5facc8ce1eb4fdcef3d24171577a7707b21
a5-stable <sh07e1916@gmail.com>
2022-09-18 12:30:32 +0900

    doc

---
+++ test.rb
@@ -0,0 +1,17 @@
+# Dentaku::Calculator.new.ast("()")
+# parser.rb:41:in `consume': undefined method `peek' for nil:NilClass (NoMethodError)
+
+# Dentaku::Calculator.new.ast("(())")
+# parser.rb:44:in `consume': undefined method `min_param_count' for Dentaku::AST::Grouping:Class (NoMethodError)
+
+# Dentaku::Calculator.new.ast("(})")
+# parser.rb:44:in `consume': undefined method `min_param_count' for Dentaku::AST::Grouping:Class (NoMethodError)
+
+# Dentaku::Calculator.new.ast("}")
+# parser.rb:341:in `fail!': Unbalanced bracket (Dentaku::ParseError)
+
+# Dentaku::Calculator.new.ast("{")
+# parser.rb:341:in `fail!': Invalid statement (Dentaku::ParseError)
+
+# Dentaku::Calculator.new.ast("({)")
+# parser.rb:341:in `fail!': Invalid statement (Dentaku::ParseError)
