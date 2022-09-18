61168f8ad1855a068eb37547cb5685a3a18632cd
a5-stable <sh07e1916@gmail.com>
2022-09-18 12:37:29 +0900

    update doc

--- test.rb
+++ test.rb
@@ -1,6 +1,9 @@
 # Dentaku::Calculator.new.ast("()")
 # parser.rb:41:in `consume': undefined method `peek' for nil:NilClass (NoMethodError)
 
+# Dentaku::Calculator.new.ast("2 + ()")
+# parser.rb:341:in `fail!': Dentaku::AST::Addition has too many operands (Dentaku::ParseError)
+
 # Dentaku::Calculator.new.ast("(())")
 # parser.rb:44:in `consume': undefined method `min_param_count' for Dentaku::AST::Grouping:Class (NoMethodError)
 
@@ -15,3 +18,6 @@
 
 # Dentaku::Calculator.new.ast("({)")
 # parser.rb:341:in `fail!': Invalid statement (Dentaku::ParseError)
+
+# Dentaku::Calculator.new.ast("{()}")
+# parser.rb:341:in `fail!': Unbalanced bracket (Dentaku::ParseError)
